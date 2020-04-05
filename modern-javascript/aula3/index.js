fe3a3f6f55e1c4898e25880b528c0b0de931fbbf
thitoribeiro <thito.ribeiro@hotmail.com>
2020-04-05 19:27:43 -0300

    Class 1, 2, 3 and 4 Javascript

---
+++ modern-javascript/aula3/index.js
@@ -0,0 +1,5 @@
+console.log("thito 'ribeiro'");
+console.log('thito "ribeiro"');
+console.log(`'thito' "ribeiro"`);
+
+console.log(35, 15.85, 'thito ribeiro again')
---
+++ modern-javascript/aula4/comentarios.js
@@ -0,0 +1,9 @@
+//Escrevendo um comentário de linha
+console.log('Olá mundo!');
+console.log('Olá mundo!'); //Outro comentário de linha
+//console.log('Olá mundo!');
+/*
+console.log('Olá mundo!');
+console.log('Olá mundo!');
+*/
+//Tudo que está como comentário será ignorado
