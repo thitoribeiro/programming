67a9fe088f120b4ab2883bd05a778fa38a0a6b37
thitoribeiro <thito.ribeiro@hotmail.com>
2020-04-10 17:31:46 -0300

    Class exercise

--- modern-javascript/aula13/aula13.js
+++ modern-javascript/aula13/aula13.js
@@ -1,6 +1,18 @@
 
-let string1 = "Um texto";
+let string1 = "O rato roeu a roupa do rei de roma.";
 
-console.log(string1.concat(' em um lindo dia.'));
-console.log(string1 + ' em um lindo dia.');
-console.log(`${string1} em um lindo dia.`)
+/*console.log(string1.concat(' em um lindo dia.'));
+**console.log(string1 + ' em um lindo dia.');
+**console.log(`${string1} em um lindo dia.`)
+*/
+/*console.log(string1.indexOf('o'));
+console.log(string1.lastIndexOf('o'));
+console.log(string1.match(/[a-z]/g));
+console.log(string1.search(/x/));
+console.log(string1.replace('Um', 'Outro'));
+console.log(string1.length);
+console.log(string1.slice(-5, -1))
+console.log(string1.substring(string1.length -5, string1.length -1));
+*/
+
+console.log(string1.split(' ', 3));
