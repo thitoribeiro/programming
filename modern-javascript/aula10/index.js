fd2ac06be388b0c0b3ec3f416ee95be5777793e4
thitoribeiro <thito.ribeiro@hotmail.com>
2020-04-09 22:37:39 -0300

    Class 11 exercise

--- modern-javascript/aula10/index.js
+++ modern-javascript/aula10/index.js
@@ -1,3 +1,16 @@
 const nome = 'Thito';
 const nomeMeio = "Rodrigo";
 const sobreNome = `Ribeiro`;
+const num1 = 10;
+const num2 = 10.52;
+let nomeAluno;
+let sobrenomeAluno = null;
+const aprovado = false;
+
+
+console.log(nome, nomeMeio, sobreNome);
+console.log(num1);
+console.log(num2);
+console.log(nomeAluno);
+console.log(sobrenomeAluno);
+console.log(aprovado);
---
+++ modern-javascript/aula11/index.js
@@ -0,0 +1,23 @@
+/**
+ *  Aritméticos
+ *  + Adição / Concatenação
+ *  - Subtração
+ *  / Divisão
+ *  * Multiplicação
+ *  ** Potenciação
+ *  % Resto da divisão
+ *  Precedência de operadores aritméticos (), **, *, / e % e depois + e -
+ *  Incremento = ++
+ *  Decremento = --
+ *  = Operador de atribuição
+ *  NaN Not a Number
+ */
+
+let = cont = 1;
+cont++;
+console.log(cont);
+
+const num1 = 1;
+const num2 = Number('Thito');
+
+console.log(num1+num2);
