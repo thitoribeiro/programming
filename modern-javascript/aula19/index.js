3cad92eee5cc06c1945f1bc6b508225bbc5bf828
thitoribeiro <thito.ribeiro@hotmail.com>
2020-04-12 20:41:46 -0300

    Class 1 and 2

---
+++ modern-javascript/aula19/index.js
@@ -0,0 +1,17 @@
+/*
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol)
+*/
+
+let nome = 'Thito';
+nome = 'Rodrigo';
+console.log(nome);
+
+/*
+Referencia (mutável) - array, object, function
+*/
+
+let a= [1, 2, 3];
+let b = a;
+ console.log(a, b);
+
+ 
---
+++ treinaweb-javascript/aula1/script.js
@@ -0,0 +1,8 @@
+
+alert('Hello World!');
+console.log('Hello World!');
+//Comentário de linha
+/*Comentário
+**de multiplas
+**linhas
+*/
