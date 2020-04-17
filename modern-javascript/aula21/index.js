dce7ecf75928dcac9b4cc5aad62faa24acae9128
thitoribeiro <thito.ribeiro@hotmail.com>
2020-04-16 21:41:01 -0300

    Class 21 and 22

---
+++ modern-javascript/aula21/index.js
@@ -0,0 +1,12 @@
+/*
+Operadores de comparação
+>   maior que
+>=  maior que ou igual
+<   menor que
+<=  menor que ou igual
+==  igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valoe e tipo)
+*/
+
---
+++ modern-javascript/aula22/index.js
@@ -0,0 +1,9 @@
+/*
+Operadores Lógicos
+&& -> AND Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR 
+! -> NOT
+*/
+
+const expressao = true && true && false && true;
+console.log(expressao);
