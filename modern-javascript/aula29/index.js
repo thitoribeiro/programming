5ccaad5969cbf2e893dfa6d0e3487097fc238c7d
thitoribeiro <thito.ribeiro@hotmail.com>
2020-04-27 21:33:25 -0300

    switch case class

---
+++ modern-javascript/aula29/index.js
@@ -0,0 +1,35 @@
+function getDayWeekText (diaSemana){
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto; 
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+            default:
+                diaSemanaTexto = '';
+        }
+}
+
+const data = new Date('1987-09-13 00:00:00');
+let diaSemana = data.getDay();
+const diaSemanaTexto = getDayWeekText(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
