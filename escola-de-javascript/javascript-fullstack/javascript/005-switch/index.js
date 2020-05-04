589dbec0bf6013f644b6ab483db6fc869edd97db
thitoribeiro <thito.ribeiro@hotmail.com>
2020-05-03 21:19:17 -0300

    switch class

---
+++ escola-de-javascript/javascript-fullstack/javascript/005-switch/index.js
@@ -0,0 +1,12 @@
+let velocity = 50
+
+switch(velocity * 2) {
+    case 100:
+        console.log('Sua velocidade é 100km/s')
+        break
+    case 160:
+        console.log('Sua velocidade é 160km/s')
+        break
+    default:
+        console.log('Velocidade não identificada')
+}
