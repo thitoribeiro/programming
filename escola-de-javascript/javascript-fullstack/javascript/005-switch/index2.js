035375e3b2419166c544735b2bdc336ad97754f4
thitoribeiro <thito.ribeiro@hotmail.com>
2020-05-04 22:25:39 -0300

    switch case class

---
+++ escola-de-javascript/javascript-fullstack/javascript/005-switch/index2.js
@@ -0,0 +1,15 @@
+let spaceship = 'Elemental'
+
+switch (spaceship) {
+    case 'Golias':
+        console.log('Nave mais resistente')
+        break
+    case 'Raptor':
+        console.log('Nave mais rápida')
+        break
+    case 'Enterprise':
+        console.log('Nave da frota estelar')
+        break
+    default:
+        console.log('Nave comum')
+}
---
+++ escola-de-javascript/javascript-fullstack/javascript/005-switch/index3.js
@@ -0,0 +1,18 @@
+let velocity = 110
+
+switch(velocity) {
+    case 80:
+        console.log('Velocidade mínima')
+        break
+    case 90:
+        console.log('Velocidade 90km/s')
+        break
+    case 100:
+        console.log('Velocidade aceitável')
+        break
+    case 110:
+        console.log('Velocidade alta, mas aceitável')
+        break
+    default:
+        console.log('Velocidade não identificada')
+}
