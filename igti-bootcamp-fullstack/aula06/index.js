68a85f58ecd87b475a51fa6d7d9050b790d00b68
thitoribeiro <thito.ribeiro@hotmail.com>
2020-05-17 17:35:22 -0300

    If else e switch case

---
+++ igti-bootcamp-fullstack/aula06/index.js
@@ -0,0 +1,48 @@
+
+var a = 6;
+var b = 6;
+
+if (a > b) {
+  console.log(a + " é maior que " + b);
+} else if (a < b) {
+  console.log(a + " é menor que " + b);
+} else {
+  console.log(a + " é igual a " + b);
+}
+
+var dia = 7;
+
+if (dia === 1) {
+  console.log("Domingo");
+} else if (dia === 2) {
+  console.log("Segunda");
+} else if (dia === 3) {
+  console.log("Terça");
+} else if (dia === 4) {
+  console.log("Quarta");
+} else if (dia === 5) {
+  console.log("Quinta");
+} else if (dia === 6) {
+  console.log("Sexta");
+} else if (dia === 7) {
+  console.log("Sábado");
+} else {
+  console.log("Inválido");
+}
+
+
+var dia = 3;
+
+switch (dia) {
+  case 1: r = 'Domingo'; break;
+  case 2: r = 'Segunda'; break;
+  case 3: r = 'Terça'; break;
+  case 4: r = 'Quarta'; break;
+  case 5: r = 'Quinta'; break;
+  case 6: r = 'Sexta'; break;
+  case 7: r = 'Sábado'; break;
+
+  default: r = 'Inválido';
+}
+
+console.log(r);
