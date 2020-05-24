b77217e79ed7f6c6c14eac0f1be94bf17dcccf55
thitoribeiro <thito.ribeiro@hotmail.com>
2020-05-24 15:00:41 -0300

    finalizando rádio podcast

--- igti-bootcamp-fullstack/capitulo01/interativa01/js/script.js
+++ igti-bootcamp-fullstack/capitulo01/interativa01/js/script.js
@@ -9,11 +9,13 @@ function start () {
     inputRange = document.querySelector('#inputRange');
     divPodcasts = document.querySelector('#divPodcasts');
 
-    inputRange.value = '87';
-    inputFrequency.value = '200';
-    divPodcasts.innerHTML = 'Hello!';
+    inputRange.value = '87.5';
+    inputFrequency.value = '87.5';
+    divPodcasts.innerHTML = 'Olá';
 
-    inputRange.addEventListener('input', handleRangeChange);  
+    inputRange.addEventListener('input', handleRangeChange);
+
+    handlePodcastFrom(inputRange.value);
 }
 
 function handleRangeChange(event) {
@@ -26,27 +28,49 @@ function handleRangeChange(event) {
 function handlePodcastFrom(frequency) {
   
   var foundPodcast = null;
-  //console.log(realPodcasts);
-  for(var i = 0; i < realPodcasts.length; i++) {
-    var currentPodcast = realPodcasts[i];
+ 
+  //for(var i = 0; i < realPodcasts.length; i++) {
+    //var currentPodcast = realPodcasts[i];
 
-    if (currentPodcast.id === frequency) {
-      foundPodcast = currentPodcast;
-      break;
-    }
-  }
+    //if (currentPodcast.id === frequency) {
+      //foundPodcast = currentPodcast;
+      //break;
+    //}
+  //}
 
-  if (foundPodcast) {
-    renderPodcasts(foundPodcast);
-  } else {
-      divPodcasts.innerHTML = 'Nenhum podcast encontrado!';
-  }
+  foundPodcast = realPodcasts.find(function(currentPodcast){
+    return currentPodcast.id === frequency;
+  })
 
+  renderPodcasts(foundPodcast);
 }
 
 function renderPodcasts(podcast) {
-  const img = document.createElement('img');
-  img.src = './img/' + podcast.img;
 
-  divPodcasts.appendChild(img);
+  if (!podcast) {
+    divPodcasts.innerHTML = 'Nenhum podcast encontrado!';
+    return;
+  }
+
+  //var img = document.createElement('img');
+  //img.src = './img/' + podcast.img;
+
+  //var title = document.createElement('h2');
+  //title.textContent = podcast.title;
+
+  //var description = document.createElement('p');
+  //description.textContent = podcast.description;
+
+  //divPodcasts.innerHTML = '';
+  //divPodcasts.appendChild(img);
+  //divPodcasts.appendChild(title);
+  //ivPodcasts.appendChild(description);
+
+  var { img, title, description } = podcast; 
+
+  divPodcasts.innerHTML = `
+  <img src='./img/${img}'/>
+  <h2>${title}</h2>
+  <p>${description}</p>
+  `;
 }
