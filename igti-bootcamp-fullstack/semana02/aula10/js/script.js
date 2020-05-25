0af1612c1cb6a99da4a6071f7a27a0e8f198318e
thitoribeiro <thito.ribeiro@hotmail.com>
2020-05-24 22:22:33 -0300

    function e arrow functions

--- igti-bootcamp-fullstack/semana02/aula10/js/script.js
+++ igti-bootcamp-fullstack/semana02/aula10/js/script.js
@@ -41,3 +41,27 @@ function withLet() {
   console.log(d);
   //d = 1;
 
+//function
+//funcao
+function sum(a, b) {
+  return a + b;
+}
+//funcao anonima
+const sum2 = function(a, b) {
+  return a + b;
+};
+
+//arrow function
+const sum3 = (a, b) => {
+  return a + b;
+};
+
+//arrow function reduzida
+const sum4 = (a, b) => a + b;
+
+console.log(sum(2, 3));
+console.log(sum2(2, 3));
+console.log(sum3(2, 3));
+console.log(sum4(2, 3));
+
+
