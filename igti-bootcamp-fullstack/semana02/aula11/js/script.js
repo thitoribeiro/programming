ab3974520f50b1bc9d6d894b2b67d66d2190c2fc
thitoribeiro <thito.ribeiro@hotmail.com>
2020-05-26 19:25:52 -0300

    map, filter e forEach

--- igti-bootcamp-fullstack/semana02/aula11/js/script.js
+++ igti-bootcamp-fullstack/semana02/aula11/js/script.js
@@ -1,23 +1,39 @@
 window.addEventListener("load", () => {
   doMap();
   doFilter();
-
+  doForEach();
 });
 //método imutável map===============================================
 function doMap() {
-  const nameEmailArray = people.results.map((person) => {
+  const nameEmailGenderArray = people.results.map((person) => {
     return {
       name: person.name,
       email: person.email,
+      gender: person.gender
     };
   });
 
-  console.log(nameEmailArray);
+  console.log(nameEmailGenderArray);
+
+  return nameEmailGenderArray;
 }
 //método imutável filter=============================================
 function doFilter() {
-  const olderThan50 = people.results.filter(person => {
-    return person.dob.age > 50; 
+  const olderThan50 = people.results.filter((person) => {
+    return person.dob.age > 50;
   });
-  console.log(olderThan50)
+  console.log(olderThan50);
 }
+//método forEach
+function doForEach() {
+  const mappedPeople = doMap();
+
+  mappedPeople.forEach((person) => {
+    person.nameSize =
+      person.name.title.length +
+      person.name.first.length +
+      person.name.last.length;
+  });
+  console.log(mappedPeople);
+}
+//método
