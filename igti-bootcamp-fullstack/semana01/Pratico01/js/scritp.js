4f820a462e09658803affe743b5a618864d7964e
thitoribeiro <thito.ribeiro@hotmail.com>
2020-05-31 17:55:03 -0300

    finalizando projeto pratico

--- igti-bootcamp-fullstack/semana01/Pratico01/js/scritp.js
+++ igti-bootcamp-fullstack/semana01/Pratico01/js/scritp.js
@@ -6,26 +6,56 @@ let inputTextR = null;
 let inputTextG = null;
 let inputTextB = null;
 
-let divSquare = null;
+//let divSquare = null;
 
-window.addEventListener('load', () => {
+let r = "0";
+let g = "0";
+let b = "0";
+
+window.addEventListener("load", () => {
   mapElements();
+  sync();
 });
 
 function mapElements() {
-  inputRangeR = document.querySelector('#inputRangeR');
-  inputRangeG = document.querySelector('#inputRangeG');
-  inputRangeB = document.querySelector('#inputRangeB');
-  
-  inputTextR = document.querySelector('#inputTextR');
-  inputTextG = document.querySelector('#inputTextG');
-  inputTextB = document.querySelector('#inputTextB');
+  inputRangeR = document.querySelector("#inputRangeR");
+  inputRangeG = document.querySelector("#inputRangeG");
+  inputRangeB = document.querySelector("#inputRangeB");
 
-  dicSquare = document.querySelector('#divSquare');
+  inputTextR = document.querySelector("#inputTextR");
+  inputTextG = document.querySelector("#inputTextG");
+  inputTextB = document.querySelector("#inputTextB");
 
-  inputTextR.value = '254';
-  inputRangeR.value = '155';
+  divSquare = document.querySelector("#divSquare");
 
-  divSquare.style.backgroundColor = 'yellow';
-  
+  inputRangeR.addEventListener("change", handleRangeChange);
+  inputRangeG.addEventListener("change", handleRangeChange);
+  inputRangeB.addEventListener("change", handleRangeChange);
+}
+
+function handleRangeChange(event) {
+  const value = event.target.value;
+  const id = event.target.id;
+
+  switch (id) {
+    case "inputRangeR":
+      r = value;
+      break;
+    case "inputRangeG":
+      g = value;
+      break;
+    case "inputRangeB":
+      b = value;
+      break;
+  }
+
+  sync();
+}
+
+function sync() {
+  divSquare.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+  inputTextR.value = r;
+  inputTextG.value = g;
+  inputTextB.value = b;
 }
