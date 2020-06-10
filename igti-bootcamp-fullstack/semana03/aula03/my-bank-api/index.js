33fd9ab0980c3a4df2c8cb5f19b01a1cd7fe6b6f
thitoribeiro <thito.ribeiro@hotmail.com>
2020-06-09 22:08:57 -0300

    fileSystem

--- igti-bootcamp-fullstack/semana03/aula03/my-bank-api/index.js
+++ igti-bootcamp-fullstack/semana03/aula03/my-bank-api/index.js
@@ -1,12 +1,26 @@
 var express = require("express");
+var fs = require("fs");
+
 var app = express();
 
-// app.get("/", function(req, res) {
-//     res.send("Hello World2!");
-// });
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.send("Hello World2!");
+});
 
 app.post("/account", (req, res) => {
+    let params = req.body;
     console.log("post account");
+
+    // fs.writeFile("accounts.json", JSON.stringify(params), err => {
+    //     console.log(err);
+    // });
+    fs.appendFile("accounts.json", JSON.stringify(params), err => {
+        console.log(err);
+    });
+    
+    res.send("post account");
 });
 
 app.listen(3000, function() {
