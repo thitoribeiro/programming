bbee9c7d1ac12c0923611a550b133549eacf6cd1
thitoribeiro <thito.ribeiro@hotmail.com>
2020-06-11 23:55:14 -0300

    get request id

--- igti-bootcamp-fullstack/semana03/aula03/my-bank-api/index.js
+++ igti-bootcamp-fullstack/semana03/aula03/my-bank-api/index.js
@@ -1,61 +1,25 @@
 var express = require("express");
 var fs = require("fs");
-
 var app = express();
+var accountsRouter = require("./routes/accounts.js");
+
+global.fileName = "accounts.json";
 
 app.use(express.json());
-
-app.post("/account", (req, res) => {
-  let account = req.body;
-  fs.readFile("accounts.json", "utf8", (err, data) => {
-    if (!err) {
-      try {
-        let json = JSON.parse(data);
-
-        account = { id: json.nextId++, ...account };
-        json.accounts.push(account);
-
-        fs.writeFile("accounts.json", JSON.stringify(json), (err) => {
-          if (err) {
-            res.status(400).send({ error: err.message });
-          } else {
-            res.end();
-          }
-        });
-      } catch (err) {
-        res.status(400).send({ error: err.message });
-      }
-    } else {
-      res.status(400).send({ error: err.message });
-    }
-  });
-});
-
-app.get("/account", (_, res) => {
-  fs.readFile("accounts.json", "utf8", (err, data) => {
-    if (!err) {
-      let json = JSON.parse(data);
-      delete json.nextId;
-      res.send(json);
-    } else {
-        res.status(400).send({ error: err.message });
-    }
-  })
-});
+app.use("/account", accountsRouter);
 
 app.listen(3000, function () {
   try {
-    fs.readFile("accounts.json", "utf8", (err, data) => {
+    fs.readFile(global.fileName, "utf8", (err, data) => {
       if (err) {
         const initialJson = {
           nextId: 1,
           accounts: [],
         };
-        fs.writeFile("accounts.json", JSON.stringify(initialJson), (err) => {
+        fs.writeFile(global.fileName, JSON.stringify(initialJson), (err) => {
           if (err) {
             console.log(err);
           }
-          
         });
       }
     });
