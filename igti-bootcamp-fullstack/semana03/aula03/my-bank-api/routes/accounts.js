58eb3b79e082255c57846ecae476e52d0410f7da
thitoribeiro <thito.ribeiro@hotmail.com>
2020-06-13 22:47:43 -0300

    put and post methods

--- igti-bootcamp-fullstack/semana03/aula03/my-bank-api/routes/accounts.js
+++ igti-bootcamp-fullstack/semana03/aula03/my-bank-api/routes/accounts.js
@@ -62,23 +62,79 @@ router.get("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   fs.readFile(global.fileName, "utf8", (err, data) => {
     try {
-        if (err) throw err;
-        
-        let json = JSON.parse(data);
-        let accounts = json.accounts.filter(account => account.id !== parseInt(req.params.id, 10));
-        json.accounts = accounts;
+      if (err) throw err;
 
-        fs.writeFile(global.fileName, JSON.stringify(json), (err) => {
-          if (err) {
-            res.status(400).send({ error: err.message });
-          } else {
-            res.end();
-          }
-        });
-      } catch (err) {
-        res.status(400).send({ error: err.message });
-      }
+      let json = JSON.parse(data);
+      let accounts = json.accounts.filter(
+        account => account.id !== parseInt(req.params.id, 10)
+      );
+      json.accounts = accounts;
+
+      fs.writeFile(global.fileName, JSON.stringify(json), (err) => {
+        if (err) {
+          res.status(400).send({ error: err.message });
+        } else {
+          res.end();
+        }
+      });
+    } catch (err) {
+      res.status(400).send({ error: err.message });
+    }
   });
+});
+
+router.put("/", (req, res) => {
+  let newAccount = req.body;
+  fs.readFile(global.fileName, "utf8", (err, data) => {
+    try {
+      if (err) throw err;
+
+      let json = JSON.parse(data);
+      let oldIndex = json.accounts.findIndex(
+        (account) => account.id === newAccount.id
+      );
+      json.accounts[oldIndex].name = newAccount.name;
+      json.accounts[oldIndex].balance = newAccount.balance;
+
+      fs.writeFile(global.fileName, JSON.stringify(json), (err) => {
+        if (err) {
+          res.status(400).send({ error: err.message });
+        } else {
+          res.end();
+        }
+      });
+    } catch (err) {
+      res.status(400).send({ error: err.message });
+    }
+  });
+});
+
+router.post("/transaction", (req, res) => {
+  let params = req.body;
+  fs.readFile(global.fileName, "utf8", (err, data) => {
+    try {
+      if (err) throw err;
+
+      let json = JSON.parse(data);
+      let index = json.accounts.findIndex(account => account.id === params.id);
+
+      if ((params.value < 0) && ((json.accounts[index].balance + params.value) < 0)) {
+        throw new Error("Não há saldo suficiente.")
+      }
+
+      json.accounts[index].balance += params.value;
+
+      fs.writeFile(global.fileName, JSON.stringify(json), err => {
+        if (err) {
+          res.status(400).send({ error: err.message });
+        } else {
+          res.send(json.accounts[index]);
+        }
+      });
+    } catch (err) {
+      res.status(400).send({ error: err.message });
+    }
+});
 });
 
 module.exports = router;
