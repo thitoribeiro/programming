30aac1c145715d7b66c3db49f49e632bb5af35cc
thitoribeiro <thito.ribeiro@hotmail.com>
2020-06-20 21:12:31 -0300

    Continuação class components parte 1

--- igti-bootcamp-fullstack/semana04/aula05/react-counter-01/src/App.js
+++ igti-bootcamp-fullstack/semana04/aula05/react-counter-01/src/App.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import React, { Component, Fragment } from "react";
+import Counter from "./components/counter/counter";
 
 export default class App extends Component {
   render() {
-    return <ProjetoBase />;
+    return (
+      <Fragment>
+        <Counter />
+        <Counter />
+        <Counter />
+      </Fragment>
+    );
   }
 }
