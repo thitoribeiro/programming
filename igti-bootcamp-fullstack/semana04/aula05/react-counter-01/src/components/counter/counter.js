228ed5bf50722ba30a54299c1115d3b8d6fc4263
thitoribeiro <thito.ribeiro@hotmail.com>
2020-06-20 22:23:36 -0300

    Class components class part 2

--- igti-bootcamp-fullstack/semana04/aula05/react-counter-01/src/components/counter/counter.js
+++ igti-bootcamp-fullstack/semana04/aula05/react-counter-01/src/components/counter/counter.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 
+import css from './counter.module.css'
+
 export default class counter extends Component {
+  constructor() {
+    super();
+
+    this.currentCounter = 2;
+  }
+
+  handleClick = () => {
+      console.log('cick');
+  }
+
   render() {
-    return <div>Counter</div>;
+    return (
+      <div className={css.counterContainer}>
+        <button 
+          onClick={this.handleClick}
+          className="waves-effect waves-light bnt red darken-4">-</button>
+        <span className={css.counterValue}>{this.currentCounter}</span>
+        <button className="waves-effect waves-light bnt green darken-4">+</button>
+      </div>
+    );
   }
 }
