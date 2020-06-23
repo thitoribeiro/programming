41f0846c5db18cfb2e22a988dd36281c567654f3
thitoribeiro <thito.ribeiro@hotmail.com>
2020-06-22 21:00:24 -0300

    Class components part 2

--- igti-bootcamp-fullstack/semana04/aula05/react-counter-01/src/components/counter/counter.js
+++ igti-bootcamp-fullstack/semana04/aula05/react-counter-01/src/components/counter/counter.js
@@ -1,26 +1,57 @@
 import React, { Component } from "react";
 
-import css from './counter.module.css'
+import css from "./counter.module.css";
 
-export default class counter extends Component {
+export default class Counter extends Component {
   constructor() {
     super();
 
-    this.currentCounter = 2;
+    this.state = {
+      currentCounter: 2,
+      steps: 0,
+    };
   }
 
-  handleClick = () => {
-      console.log('cick');
-  }
+  handleButtonDownClick = () => {
+    const { currentCounter, steps } = this.state;
+
+    this.setState({
+      currentCounter: currentCounter - 1,
+      steps: steps + 1,
+    });
+  };
+
+  handleButtonUpClick = () => {
+    const { currentCounter, steps } = this.state;
+
+    this.setState({
+      currentCounter: currentCounter + 1,
+      steps: steps + 1,
+    });
+  };
 
   render() {
+    const { currentCounter, steps } = this.state;
+
     return (
       <div className={css.counterContainer}>
-        <button 
-          onClick={this.handleClick}
-          className="waves-effect waves-light bnt red darken-4">-</button>
-        <span className={css.counterValue}>{this.currentCounter}</span>
-        <button className="waves-effect waves-light bnt green darken-4">+</button>
+        <button
+          onClick={this.handleButtonDownClick}
+          className="waves-effect waves-light bnt red darken-4"
+        >
+          -
+        </button>
+
+        <span className={css.counterValue}>{currentCounter}</span>
+
+        <button
+          onClick={this.handleButtonUpClick}
+          className="waves-effect waves-light bnt green darken-4"
+        >
+          +
+        </button>
+
+        <span className={css.counterValue}>({steps})</span>
       </div>
     );
   }
